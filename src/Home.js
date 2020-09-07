b2e641d11dc51b46849a12bc2c129c321928529b
deepak1995p <deepak1995p@users.github.com>
2020-09-08 00:49:26 +0530

    added the products in Product Component

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,69 @@
 import React from 'react'
 import './Home.css'
-import HomeImage from './try_amazon_prime.jpg'
+import HomeImage from './home_image.jpg'
+import Backpack from './assets/backpack2.jpg'
+import camera from './assets/sonycamera.jpg'
+import washingMachine from './assets/washing_machine.jpg'
 import Product from './Product'
 
 function Home() {
     return (
         <div className="home">
-            <img 
-            className="home_image"
-            src=  {HomeImage}
-            alt="prime_image"
+            <img
+                className="home_image"
+                src={HomeImage}
+                alt="prime_image"
             />
-            
+
             {/* Product id, title,price,rating,image */}
-            <Product
-                id="123456"
-                title="The Lean Startup: How constant Innovation Creates"
-                price={100}
-                rating={5}
-                image= ""
-            />
+            <div className="home_row">
+                <Product
+                    id="123456"
+                    title="Samsung 6.0 Kg Fully-Automatic 5 Star Front Loading Washing Machine (WW60R20GLMA/TL, White)"
+                    price={12000}
+                    rating={5}
+                    image={washingMachine}
+                />
+                <Product
+                    id="123456"
+                    title="American Tourister 32 Ltrs Black Casual Backpack (AMT FIZZ SCH BAG 02 - BLACK)"
+                    price={12000}
+                    rating={5}
+                    image={Backpack}
+                />
+            </div>
+            <div className="home_row">
+                <Product
+                    id="123456"
+                    title="Sony DSC W830 Cyber-Shot 20.1 MP Point and Shoot Camera (Black) with 8X Optical Zoom"
+                    price={12000}
+                    rating={5}
+                    image={camera}
+                />
+                <Product
+                    id="123456"
+                    title="Samsung 6.0 Kg Fully-Automatic 5 Star Front Loading Washing Machine (WW60R20GLMA/TL, White)"
+                    price={12000}
+                    rating={5}
+                    image={washingMachine}
+                />
+                <Product
+                    id="123456"
+                    title="Samsung 6.0 Kg Fully-Automatic 5 Star Front Loading Washing Machine (WW60R20GLMA/TL, White)"
+                    price={12000}
+                    rating={5}
+                    image={washingMachine}
+                />
+            </div>
+            <div className="home_row">
+                <Product
+                    id="123456"
+                    title="Samsung 6.0 Kg Fully-Automatic 5 Star Front Loading Washing Machine (WW60R20GLMA/TL, White)"
+                    price={12000}
+                    rating={5}
+                    image={washingMachine}
+                />
+            </div>
         </div>
     )
 }
--- src/Product.js
+++ src/Product.js
@@ -4,17 +4,19 @@ import './Product.css'
 function Product({ id, title, image, price, rating }) {
     return (
         <div className="product">
-            <p>{title}</p>
-            <p className="product_price">
-                <small>₹</small>
-                <strong>{price}</strong>
-            </p>
-            <div className="product_rating">
-                {Array(rating)
-                    .fill()
-                    .map(() => (
-                       <p>⭐️</p>
-                    ))}
+            <div className="product_info">
+                <p>{title}</p>
+                <p className="product_price">
+                    <small>₹</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className="product_rating">
+                    {Array(rating)
+                        .fill()
+                        .map(() => (
+                            <p>⭐️</p>
+                        ))}
+                </div>
             </div>
             <img src={image} alt="" />
             <button>Add to Basket</button>
